f61e1cd10c0608410edb8c3aa98e0f70caaed647
strahinjaobradovicso <strahinjaobradovicso@gmail.com>
2024-01-11 17:23:48 +0100

    added types to the function parameters

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,6 +1,7 @@
-import { ErrorRequestHandler } from "express";
+import { NextFunction, Request, Response } from "express";
+import { HttpException } from "../exceptions/httpException";
 
-const errorMiddleware: ErrorRequestHandler = (error, req, res, next)=>{
+const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
 
     const status: number = error.status || 500;
     const message: string = error.message || '';
