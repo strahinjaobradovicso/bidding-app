1eb48019fa2a1a8933c7d6a9838d21cfb3de0b40
strahinjaobradovicso <strahinjaobradovicso@gmail.com>
2024-01-20 15:28:55 +0100

    user service test updated

--- src/tests/services/userService.test.ts
+++ src/tests/services/userService.test.ts
@@ -1,47 +1,50 @@
 import DB from "../../database"
 import { UserModel } from "../../database/models/user"
 import { CreateUserDto } from "../../dtos/user"
-import { HttpException } from "../../exceptions/httpException"
 import { UserService } from "../../services/UserService"
 
 const userService = new UserService()
 
-describe('user service', ()=>{
+describe('user service', () => {
 
     afterAll(()=>{
         DB.sequelize.close()
     })
 
-    let user: CreateUserDto = {
-        username: `user${new Date()}`,
-        password: '',
-        email: `email${new Date()}`
-    }
+    let user: CreateUserDto;
+    let copy: CreateUserDto;
 
-    let copy = { ...user }
+    beforeEach(() => {
+        let sufix = `${new Date().valueOf()}`
+        user = {
+            username: `user_${sufix}`,
+            password: '',
+            email: `email_${sufix}`
+        }
+    })
 
-    it('unique email and password => create and return user', (done) => {
+    it('unique email and password => create and return user model instance', (done) => {
         userService.createUser(user)
         .then(savedUser=>{
             expect(savedUser).toBeInstanceOf(UserModel)
             done()
-        })
-        .catch(err=>{
-            throw new Error('this error should not be thrown')
+            copy = {...user}
         })
     })
 
     it('not unique email and password => throw HttpException instance', (done) => {
         userService.createUser(copy)
-        .then(savedCopy=>{
-            throw new Error('this error should not be thrown')
-        })
         .catch(err=>{
-            expect(err).toBeInstanceOf(HttpException)
-            if(err instanceof HttpException){
-                expect(err.status).toBe(409)
-                expect(err.message).toEqual('username already exists, email already exists')
-            }
+            expect(err.status).toBe(409)
+            expect(err.message).toEqual('username already exists, email already exists')
+            done()
+        })
+    })
+
+    it('createUser should return user without sensitive data', (done) => {   
+        userService.createUser(user).then(user=>{
+            expect(user.password).toBeUndefined()
+            expect(user.salt).toBeUndefined()
             done()
         })
     })
