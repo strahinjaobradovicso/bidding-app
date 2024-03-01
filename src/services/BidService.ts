4e82d044131b99324cb1954c6820a9f94ccdd3df
strahinjaobradovicso <strahinjaobradovicso@gmail.com>
2024-03-01 10:24:55 +0100

    bid service to class

--- src/services/BidService.ts
+++ src/services/BidService.ts
@@ -3,18 +3,19 @@ import { BidModel } from "../database/models/bid";
 import { CreateBidDto } from "../dtos/bid";
 import { HttpException } from "../exceptions/httpException";
 
-const dbBid = DB.Bid;
 
-const createBid = async (bidData: CreateBidDto): Promise<BidModel> => {
-    let bid;
-    try {
-        bid = await dbBid.create(bidData);
-    } catch (error) {
-        throw new HttpException(500);
+export class BidService {
+
+    private dbBid = DB.Bid;
+    
+    public createBid = async (bidData: CreateBidDto): Promise<BidModel> => {
+        let bid;
+        try {
+            bid = await this.dbBid.create(bidData);
+        } catch (error) {
+            throw new HttpException(500);
+        }
+        return bid;
     }
-    return bid;
-}
 
-export const bidService = {
-    createBid
 }
