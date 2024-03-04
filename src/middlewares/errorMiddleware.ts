866e970513f83311ae64f6145bcb2c796533b59e
strahinjaobradovicso <strahinjaobradovicso@gmail.com>
2024-03-04 12:52:01 +0100

    only expected errors in error middleware

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -3,6 +3,9 @@ import { HttpException } from "../exceptions/httpException";
 
 const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
 
+    if(!error.status){
+        error = new HttpException(500);
+    }
     const status: number = error.status;
     const message: string = error.message;
 
